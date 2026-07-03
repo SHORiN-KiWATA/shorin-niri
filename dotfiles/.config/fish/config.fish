4be9b82efec1858e95b32de8cb436ca7dac50dfe
shorinkiwata <fcl709@outlook.com>
2026-07-03 23:53:35 +0900

    cleanup: remove 安装/卸载 fish function

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -43,15 +43,6 @@ function raw
 	command ~/.config/scripts/random-anime-wallpaper.sh $argv
 end
 
-function 安装
-	command yay -S $argv
-end
-
-function 卸载
-	command yay -Rns $argv
-end 
-
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/shorin/.lmstudio/bin
 # End of LM Studio CLI section
-
